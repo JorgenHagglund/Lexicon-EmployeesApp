58e9e0318d3e9533613bac1aa491df5066d3c0a0
Jörgen Hägglund <jorgen@sirlucifer.mine.nu>
2025-05-15 11:23:40 +0200

    Add project files.

---
+++ EmployeesApp.Web/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Web.Models;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Full Name", Prompt = "Name")]
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; }
+
+    [Display(Name = "E-mail address", Prompt = "E-mail address")]
+    [EmailAddress(ErrorMessage = "E-mail address is invalid")]
+    public string Email { get; set; }
+}
